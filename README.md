6633b36a9790c373e3332d6896b37a4db16d853a
Artur Ribeiro <profissional.arturrc@gmail.com>
2023-01-06 21:00:37 -0300

    adjust

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # portal_de_noticias_nodejs
+
+
+<!-- https://xd.adobe.com/view/a88b359f-f036-440b-9f1b-3c4bd41a3dbe-fdd8/ -->
--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-teste commit gitkraken
+const express = require('express');
+var bodyParser = require('body-parser');
+
+const path  = require('path');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/pages'));
+
+app.get('/', (req, res)=> {
+
+  if(req.query.busca == null){
+    res.render('home', {})
+
+  } else {
+
+    res.send(`Você procurou por : ${req.query.busca}`)
+  }
+
+});
+
+app.get('/:slug', (req, res)=> {
+  res.send(req.params.slug);
+});
+
+app.listen(3030, () => {
+  console.log('servidor no ar')
+});
