7b17dce149dcac5854dfe907e0294968c089ab6f
Artur Ribeiro <profissional.arturrc@gmail.com>
2023-01-15 00:48:51 -0300

    Instalação do Mongoose, configuração do Banco MongoDB Atlas

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 const path  = require('path');
 
 const app = express();
-const port = 3030
+const port = 3030;
+
+
+mongoose.connect('mongodb+srv://root:Artur17118901@cluster0.izmls72.mongodb.net/?retryWrites=true&w=majority',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(function(){
+  console.log('Banco Mongodb Atlas conectado com sucesso!')
+}).catch(function(err){
+  console.log(err.message)
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
