001f7cff213aa9b1c5af885e6f85fc33871eab31
Artur Ribeiro <profissional.arturrc@gmail.com>
2023-01-16 00:55:55 -0300

    Commit da aula

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 const port = 3030;
 
 const Posts = require('./Posts');
+const { findOneAndUpdate } = require('./Posts');
 
 
 mongoose.connect('mongodb+srv://root:Artur17118901@cluster0.izmls72.mongodb.net/DankiCode?retryWrites=true&w=majority',{
@@ -42,11 +43,25 @@ app.get('/', (req, res)=> {
           categoria: val.categoria,
         }
       });
-
-      res.render('home', {posts: posts});
     });
 
+    Posts.find({}).sort({'views': -1}).limit(3).exec(function(err,postsTop){
+      postsTop = postsTop.map(function(val){
 
+        return {
+          titulo: val.titulo,
+          conteudo: val.conteudo,
+          descricaoCurta: val.conteudo.substring(0,100),
+          imagem: val.imagem,
+          slug: val.slug,
+          categoria: val.categoria,
+          views: val.views
+        }
+      })
+    })
+
+
+    res.render('home',{posts:Posts,postsTop:postsTop});
   } else {
     res.render('busca', {})
   }
@@ -55,7 +70,16 @@ app.get('/', (req, res)=> {
 
 app.get('/:slug', (req, res)=> {
   // res.send(req.params.slug);
-  res.render('single',{})
+
+  Posts.findOneAndUpdate(
+    {slug: req.params.slug},
+    {$inc : {views: 1}},
+    {new: true},
+    function (err, resposta){
+      // console.log(resposta)
+      res.render('single',{noticia: resposta})
+    }
+  )
 });
 
 app.listen(port, () => {
